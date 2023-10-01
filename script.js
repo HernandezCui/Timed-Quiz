71946a854d4b2fe35c228573388bf49f8decbb3a
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2023-10-01 16:00:10 -0700

    began the format for questions & time

---
+++ script.js
@@ -0,0 +1,15 @@
+// Quiz questions and answers 
+var questions = [
+    { 
+        questions: "Question 1?",
+        choices: ["1", "2", "3"],
+        correct: 0, // Index of correct answer in choices 
+
+    },
+
+];
+
+let currentQuestion = 0;
+let timeLeft = 60; // total time
+let timerInterval; 
+let score = 0;
