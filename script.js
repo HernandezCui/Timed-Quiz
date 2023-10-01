bcdb805df99959a0a38f5bcf92cc16fd06561f74
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2023-10-01 16:37:18 -0700

    grabbed the element ids

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ let currentQuestion = 0;
 let timeLeft = 60; // total time
 let timerInterval; 
 let score = 0;
+
+var startBtn = document.getElementById("start-button");
+var quizContainer = document.getElementById("questions");
+var questionText = document.getElementById("question-text");
+var choicesContainer = document.getElementById("choices");
+var resultsContainer = document.getElementById("end-of-quiz");
+var finalScore = document.getElementById("final-score");
+var initialsInput = document.getElementById("initials");
+var submitScoreBtn = document.getElementById("submit-button");
+var highScoreContainer = document.getElementById("scores");
+var scoresList = document.getElementById("score-list");
+var clearScoresBtn = document.getElementById("clear-scores");
+var goBackBtn = document.getElementById("return");
+
