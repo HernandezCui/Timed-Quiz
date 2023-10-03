77952118fb19cce3a5ebcafbc81c6b1415bd79f8
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2023-10-02 20:17:47 -0700

    changed global variables and start quiz function

--- script.js
+++ script.js
@@ -37,12 +37,10 @@ var questions = [
     },
 ];
 
-
+// other global variables 
 let currentQuestionIndex = 0;
 let timerInterval;
-let timeLeft = 0; // Initial time in seconds 
-var penaltyTime = 10; // Penalty for incorrect answers 
-let score = 0; 
+let timeLeft = 60; // Initial time in seconds 
 
 // DOM Elements 
 var startButton = document.getElementById("start-button");
@@ -59,14 +57,10 @@ var finalScoreElement = document.getElementById("final-score");
 startButton.addEventListener("click", startQuiz);
 
 function startQuiz() {
-    startButton.classList.add("hidden");
-    endOfQuizElement.classList.add("hidden");
-    timeLeft = 60; // Setting initial time in seconds 
-    score = 0; 
-    currentQuestionIndex = 0;
-    updateTimer();
-    nextQuestion();
-    timerInterval = setInterval(updateTimer, 1000);
+    startButton.style.display = "none";
+    questionsContainer.classList.remove("hidden");
+    setNextQuestion();
+    startTimer();
 }
 
 // Updating the timer function 
