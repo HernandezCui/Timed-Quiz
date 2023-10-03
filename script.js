9fbac7109711e5802aa92394b2b5daa91db256b2
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2023-10-02 19:21:30 -0700

    created functions

--- script.js
+++ script.js
@@ -37,4 +37,55 @@ function startQuiz() {
     timerInterval = setInterval(updateTimer, 1000);
 }
 
+// Updating the timer function 
+function updateTimer() {
+    timerElement.textContent = timeLeft;
+    if (timeLeft <= 0) {
+        endQuiz();
+    } else {
+        timeLeft--;
+    }
+}
+
+// function to display next questions 
+function nextQuestion() {
+    if (currentQuestionIndex < questions.length) {
+        var question = questions[currentQuestionIndex];
+        questionTextElement.textContent = question.question;
+        choicesElement.innerHTML = "";
+        question.choices.forEach((choice, index) => {
+            var choiceButton = document.createElement("button"); 
+            choiceButton.textContent = choice; 
+            choiceButton.addEventListener("click", () => {
+                checkAnswer(index);
+            });
+            choicesElement.appendChild(choiceButton);
+        });
+    } else {
+        endQuiz();
+    }
+}
+
+// Function to check the answer
+function checkAnswer(selectedIndex) {
+    var question = questions[currentQuestionIndex];
+    if (selectedIndex === questions.correctAnswer) {
+        score++;
+    } else {
+        timeLeft -= penaltyTime;
+    }
+    currentQuestionIndex++;
+    nextQuestion();
+}
+
+// function to end quiz 
+function endQuiz() {
+    clearInterval(timerInterval);
+    questionTextElement.textContent = "";
+    choicesElement.innerHTML = "";
+    finalScoreElement.textContent = score;
+    endOfQuizElement.classList.remove("hidden");
+}
+
+
 
