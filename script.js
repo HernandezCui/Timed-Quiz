0a9163cf03427377c56f5ce55dc9c505b9eb3294
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2023-10-02 20:14:44 -0700

    added questions

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
 // Quiz data variables 
 var questions = [
     {
-        question: "Question 1?",
-        choices: ["Option 1", "Option 2", "Option 3"],
+        question: "What does HTML stand for?",
+        choices: ["Hyper Text Markup Language", "Hyperlinks and Text Markup Language", "Home Tool Markup Language"],
         correctAnswer: 0, // Index of the correct answer
     },
+    {
+        question: "What does CSS stand for?",
+        choices: ["Creative Style Sheets", "Computer Style Sheet", "Cascading Style Sheet", "Colorful Style Sheet"],
+        correctAnswer: 2,
+    },
+    {
+        question: "String values must be enclosed within ___ when being assigned to variables",
+        choices: ["quotes", "curly brackets", "paranthesis", "commas"],
+        correctAnswer: 0,
+    },
+    {
+        question: "Commonly used data types DO NOT include:",
+        choices: ["booleans", "alerts", "strings", "numbers"],
+        correctAnswer: 1, 
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ___.",
+        choices: ["other arrays", "booleans", "numbers and strings", "all of the above"],
+        correctAnswer: 3,
+    },
+    {
+        question: "Which tag is used to underline text?",
+        choices: ["<u>", "<ul>", "<li", "<l>"],
+        correctAnswer: 0,
+    },
+    {
+        question: "HTML uses ___.",
+        choices: ["labels", "quotes", "tabs", "tags"],
+        correctAnswer: 3,
+    },
 ];
+
 
 let currentQuestionIndex = 0;
 let timerInterval;
@@ -16,6 +47,7 @@ let score = 0;
 // DOM Elements 
 var startButton = document.getElementById("start-button");
 var timerElement = document.getElementById("timer");
+var questionsContainer = document.getElementById("questions");
 var questionTextElement = document.getElementById("question-text");
 var choicesElement = document.getElementById("choices");
 var endOfQuizElement = document.getElementById("end-of-quiz");
@@ -90,8 +122,8 @@ function endQuiz() {
 // Initials submission 
 submitButton.addEventListener("click", () => {
     var initials = initialsInput.value;
-    
-})
+
+});
 
 
 
