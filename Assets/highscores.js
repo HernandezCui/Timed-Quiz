35908fb4f6632be8b461e49744b53fa15aa984b6
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2023-10-15 17:40:05 -0700

    Created highscores JS & elements

---
+++ Assets/highscores.js
@@ -0,0 +1,5 @@
+// DOM Elements
+var scoresList = document.getElementById("score-list");
+var clearScoresButton = document.getElementById("clear-scores");
+var returnButton = document.getElementById("return");
+
